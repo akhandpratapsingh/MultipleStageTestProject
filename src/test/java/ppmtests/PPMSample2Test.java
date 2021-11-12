8cb87d6abf23691af6bf96d53a45d0951f92207c
akhand pratap singh <akhand123singh@gmail.com>
2021-11-12 18:14:58 +0530

    Update PPMSample2Test.java

--- src/test/java/ppmtests/PPMSample2Test.java
+++ src/test/java/ppmtests/PPMSample2Test.java
@@ -2,12 +2,14 @@ package ppmtests;
 
 import org.junit.Assert;
 import org.junit.Test;
+import java.io.*;
 
 public class PPMSample2Test {
 
 	
     @Test 
     public void Sample2Test() { 
+	System.out.println("Hello Sample2Test");
         Assert.assertTrue(true);
     }
 
