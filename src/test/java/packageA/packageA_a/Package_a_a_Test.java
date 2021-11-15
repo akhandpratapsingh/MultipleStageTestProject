0eba7c53433a38431763c625cffbc06271d5674e
Akhand Pratap Singh <akhandpratap.singh@servicenow.com>
2021-11-15 15:03:21 +0530

    adding few classes

---
+++ src/test/java/packageA/packageA_a/Package_a_a_Test.java
@@ -0,0 +1,12 @@
+package packageA.packageA_a;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Package_a_a_Test {
+
+    @Test
+    public void package_a_a_TestCase() {
+        Assert.assertTrue(true);
+    }
+}
---
+++ src/test/java/packageA/packageA_b/Package_a_b_Test.java
@@ -0,0 +1,12 @@
+package packageA.packageA_b;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Package_a_b_Test {
+
+    @Test
+    public void package_a_b_TestCase() {
+        Assert.assertTrue(true);
+    }
+}
